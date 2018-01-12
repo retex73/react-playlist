daf9001e4f429369f2eabe46e6943ad167e272f6
Peter Williams <retex.williams@gmail.com>
2018-01-12 20:55:48 +0000

    Included storybook

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var path = require('path');
 module.exports = {
 
     entry: path.resolve(__dirname, 'src') + '/app/index.js',
-    debug: true,
     devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist') + '/app',
